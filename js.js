c50a7b4bb86d9617079f20b6991320b478ea655c
Kostyantyn Onischenko <Onischenko411@gmail.com>
2024-01-06 22:27:55 +0200

    File change

--- js.js
+++ js.js
@@ -1,27 +1,27 @@
-const event = document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.getElementById('buttonInput');
     const textInput = document.getElementById('textInput');
-    const list = document.getElementById ('list');
+    const list = document.getElementById('list');
 
-    addButton.addEventListener('click',function(){
-        const text = textInput.ariaValueMax.trim()
-        if(text !== ''){
-            addListItem (text);
-            textInput.value = ''
+    addButton.addEventListener('click', function () {
+        const text = textInput.value.trim();
+        if (text !== '') {
+            addListItem(text);
+            textInput.value = '';
         }
     });
-    list.addEventListener('click', function (event){
-    
-        if(event.target.classList.contains('deleteButton')){
+
+    list.addEventListener('click', function (event) {
+        if (event.target.classList.contains('deleteButton')) {
             event.target.closest('li').remove();
         }
     });
-    function addListItem (text){
+
+    function addListItem(text) {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
         <span>${text}</span>
-        <button class ="deleteButton">X</button>`
-        list.appendChild(listItem)
+        <button class="deleteButton">X</button>`;
+        list.appendChild(listItem);
     }
-    
 });
